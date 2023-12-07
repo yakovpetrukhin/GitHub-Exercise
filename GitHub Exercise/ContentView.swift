c1a1e10add58169d34a77a190f0cd43aaed75ac1
Yakov Petrukhin <yakov.petrukhin@gmail.com>
2023-12-06 20:21:33 -0500

    updated main view

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black
+                )
         }
         .padding()
+        .foregroundStyle(.red)
     }
 }
 
