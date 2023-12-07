ca26c45bcf11c6d17f924a40a31b9d2bf138de2b
Yakov Petrukhin <yakov.petrukhin@gmail.com>
2023-12-06 20:45:48 -0500

    version 3.0 - blue

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "2.circle")
+            Image(systemName: "3.circle")
                 .resizable()
                 .scaledToFit()
-            Text("Version 2.0")
+            Text("Version 3.0")
                 .font(.largeTitle)
                 .fontWeight(.black
                 )
         }
         .padding()
-        .foregroundStyle(.green)
+        .foregroundStyle(.blue)
     }
 }
 
